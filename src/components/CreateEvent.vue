7a078330a543c2ad0b6c8bf921f22b08848a6fa2
ewamac <>
2020-04-26 09:35:09 +0200

    Add bootstrap.

--- src/components/CreateEvent.vue
+++ src/components/CreateEvent.vue
@@ -1,34 +1,39 @@
 <template>
-	<section id="settings">
-		<div class="col1">
-			<h3>Create Event</h3>
-			<p>Create New Event</p>
-			<div class="create-post">
-				<form @submit.prevent>
-					<label for="name">Name</label>
-					<input v-model.trim="event.name" id="name" />
-					<label for="name">Place</label>
-					<input v-model.trim="event.name" id="name" />
-					<label for="name">Date</label>
-					<datepicker
-						v-model="event.date"
-						name="uniquename"
-					></datepicker>
-					<label for="name">Event Start Time</label>
-					<input type="time" id="appt" name="appt" />
-					<label for="description">Description</label>
-					<textarea v-model.trim="event.content"></textarea>
-					<button
-						@click="createPost"
-						:disabled="event.content == ''"
-						class="button"
-					>
-						Add Event
-					</button>
-				</form>
-			</div>
-		</div>
-	</section>
+  <section id="settings">
+    <div class="col1">
+      <b-alert
+        variant="success"
+        :show="dismissCountDown"
+        dismissible
+        @dismissed="dismissCountDown = 0"
+        @dismiss-count-down="countDownChanged"
+        >Event created</b-alert
+      >
+      <h3>Create Event</h3>
+      <p>Create New Event</p>
+      <div class="create-post">
+        <form @submit.prevent>
+          <label for="name">Name</label>
+          <input v-model.trim="event.name" id="name" />
+          <label for="name">Place</label>
+          <input v-model.trim="event.place" id="name" />
+          <label for="name">Date</label>
+          <datepicker v-model="event.date" name="uniquename"></datepicker>
+          <label for="name">Event Start Time</label>
+          <input v-model="event.time" type="time" id="appt" name="appt" />
+          <label for="description">Description</label>
+          <textarea v-model.trim="event.content"></textarea>
+          <button
+            @click="createPost"
+            :disabled="event.content == ''"
+            class="button"
+          >
+            Add Event
+          </button>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -36,45 +41,61 @@ import { mapState } from "vuex";
 import moment from "moment";
 const fb = require("../firebaseConfig.js");
 import Datepicker from "vuejs-datepicker";
+import { DropdownPlugin } from "bootstrap-vue";
 
 export default {
-	components: {
-		Datepicker
-	},
-	data() {
-		return {
-			event: {
-				content: "",
-				name: "",
-				place: "",
-				date: "",
-				description: ""
-			}
-		};
-	},
-	computed: {
-		...mapState(["events"])
-	},
-	methods: {
-		createPost() {
-			fb.eventsCollection
-				.add({
-					createdOn: new Date(),
-					content: this.post.content,
-					userId: this.currentUser.uid,
-					userName: this.userProfile.name,
-					comments: 0,
-					likes: 0
-				})
-				.then(ref => {
-					this.event.content = "";
-				})
-				.catch(err => {
-					console.log(err);
-				});
-		}
-	}
+  components: {
+    Datepicker,
+  },
+  data() {
+    return {
+      event: {
+        time: "",
+        name: "",
+        place: "",
+        date: "",
+        description: "",
+      },
+      dismissCountDown: 0,
+    };
+  },
+  computed: {
+    ...mapState(["userProfile", "currentUser"]),
+  },
+  methods: {
+    clearEvent() {
+      this.event = {
+        time: "",
+        name: "",
+        place: "",
+        date: "",
+        description: "",
+      };
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    createPost() {
+      fb.eventsCollection
+        .add({
+          createdOn: new Date(),
+          userId: this.currentUser.uid,
+          place: this.event.place,
+          date: this.event.date,
+          description: this.event.description,
+          time: this.event.time,
+          comments: 0,
+          likes: 0,
+          participants: 0,
+        })
+        .then((ref) => {
+          this.dismissCountDown = 4;
+          this.clearEvent();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
-
-http://dev.virtualearth.net/REST/v1/Locations/UK/{postalCode}?includeNeighborhood={includeNeighborhood}&include={includeValue}&maxResults={maxResults}&key={BingMapsAPIKey}
--- src/router/index.js
+++ src/router/index.js
@@ -7,67 +7,73 @@ import Users from "@/components/Users";
 import Dashboard from "@/components/Dashboard";
 import Settings from "@/components/Settings";
 import CreateEvent from "@/components/CreateEvent";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.use(Router);
 
 const router = new Router({
-	mode: "history",
-	routes: [
-		{
-			path: "*",
-			redirect: "/dashboard"
-		},
-		{
-			path: "/login",
-			name: "Login",
-			component: Login
-		},
-		{
-			path: "/dashboard",
-			name: "Dashboard",
-			component: Dashboard,
-			meta: {
-				requiresAuth: true
-			}
-		},
-		{
-			path: "/settings",
-			name: "Settings",
-			component: Settings,
-			meta: {
-				requiresAuth: true
-			}
-		},
-		{
-			path: "/users",
-			name: "Users",
-			component: Users,
-			meta: {
-				requiresAuth: true
-			}
-		},
-		{
-			path: "/create_event",
-			name: "CreateEvent",
-			component: CreateEvent,
-			meta: {
-				requiresAuth: true
-			}
-		}
-	]
+  mode: "history",
+  routes: [
+    {
+      path: "*",
+      redirect: "/dashboard",
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/settings",
+      name: "Settings",
+      component: Settings,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/users",
+      name: "Users",
+      component: Users,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/create_event",
+      name: "CreateEvent",
+      component: CreateEvent,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+  ],
 });
 
 router.beforeEach((to, from, next) => {
-	const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
-	const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  const currentUser = firebase.auth().currentUser;
 
-	if (requiresAuth && !currentUser) {
-		next("/login");
-	} else if (requiresAuth && currentUser) {
-		next();
-	} else {
-		next();
-	}
+  if (requiresAuth && !currentUser) {
+    next("/login");
+  } else if (requiresAuth && currentUser) {
+    next();
+  } else {
+    next();
+  }
 });
 
 export default router;
